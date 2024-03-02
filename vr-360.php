021e7030bd5f5486a487fbaf359d8657daa9348e
twwli <hello@theworldwelivein.co.uk>
2024-03-02 15:34:22 +0100

    Initial Commit

---
+++ vr-360.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Plugin Name: VR 360
+Plugin URI: https://floatmagazin.de/
+Description: A plugin for integrating 360-degree images into articles.
+Version: 1.0
+Author: Olivier Guillard
+Author URI: https://olivierguillard.dev/
+*/
+
+function mon_plugin_images_360_scripts() {
+    // Enqueue Pannellum CSS
+    wp_enqueue_style('pannellum-css', '//cdn.jsdelivr.net/npm/pannellum@2.5.6/build/pannellum.css');
+    
+    // Enqueue Pannellum JS
+    wp_enqueue_script('pannellum-js', '//cdn.jsdelivr.net/npm/pannellum@2.5.6/build/pannellum.js', array(), null, true);
+}
+add_action('wp_enqueue_scripts', 'mon_plugin_images_360_scripts');
+
+function mon_plugin_afficher_image_360($atts) {
+    static $counter = 0;
+    $attributs = shortcode_atts(array(
+        'src' => '',
+        'width' => '100%',
+        'height' => '400px',
+    ), $atts);
+    $viewer_id = 'mon-image-360-' . $counter++;
+    $output = "<div id='{$viewer_id}' style='width: {$attributs['width']}; height: {$attributs['height']};'></div>";
+    $output .= "<script>
+    pannellum.viewer('{$viewer_id}', {
+        'type': 'equirectangular',
+        'panorama': '{$attributs['src']}'
+        // Vous pouvez ajouter plus d'options ici selon les besoins
+    });
+    </script>";
+    return $output;
+}
+add_shortcode('image_360', 'mon_plugin_afficher_image_360');
